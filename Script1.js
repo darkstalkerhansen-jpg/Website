1cd1318fbc33b1ea9d7c217e776b91704af58a52
darkstalkerhansen-jpg <darkstalkerhansen@gmail.com>
2026-04-29 11:09:39 -0600

    cleaned up a little

---
+++ Script1.js
@@ -0,0 +1,54 @@
+const cerch = document.getElementById("IBig");
+cerch.style.opacity="0";
+iframee = false;
+full = false;
+
+function update() {
+    current=document.getElementById("IBig").src;
+    document.getElementById("button2").placeholder=current;
+}
+function fullscreen() {
+    const wow = document.getElementById("title-wrapper");
+    if (full===false) {
+        cerch.style.display='block';
+        cerch.style.border='none';
+        cerch.style.width='100vw';
+        cerch.style.height='100vh';
+        wow.style.display = 'none';
+        full = true;
+    } else if (full) {
+        cerch.style.border='outset';
+        cerch.style.width='500px';
+        cerch.style.height='500px';
+        wow.style.display = 'flex';
+        full = false;
+    }
+}
+function launch() {
+    if (iframee===false) { 
+        cerch.style.opacity="1";
+        const inpt = document.getElementById("button1");
+        let source = inpt.value || "Falcon.html";
+        cerch.src = source;
+        update();
+        iframee=true;
+    } else if (iframee) {
+        cerch.style.opacity='0';
+        cerch.src="";
+        iframee = false;
+    }
+}
+function Proxyy() {
+                if (iframee) {
+                    iframee = false;
+                    cerch.style="opacity:0;";
+                    cerch.src = "";
+                } else if (iframee===false) {
+                    cerch.src = "Falcon.html";
+                    cerch.style="opacity:1;"
+                    iframee = true;
+                }
+            }
+            
+            
+       
